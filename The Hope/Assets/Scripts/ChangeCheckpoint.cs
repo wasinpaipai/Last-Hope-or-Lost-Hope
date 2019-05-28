cd534b25a8c84d6f04c4460c7b7f109b4624b171
The buddy_G's <59070144@it.kmitl.ac.th>
2019-05-28 22:18:18 +0700

    Add Pause Scene
    ใส่ส่วนของการ pause ระหว่างเล่น

---
+++ The Hope/Assets/Scripts/ChangeCheckpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCheckpoint : MonoBehaviour
+{
+    public GameObject checkpoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(checkpoint);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ The Hope/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Transform checkpoint;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.position = checkpoint.position;
+        }
+    }
+}
