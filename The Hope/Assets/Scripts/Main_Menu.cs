3198dd695613f2f6ae5cf7b978fa9356e65a110b
Pai <59070155@it.kmitl.ac.th>
2019-05-28 15:38:47 +0700

    Revert "Revert "Add Opening Scene""
    This reverts commit 907dc2366b8b56190916f0808cce7d14cd476013.

---
+++ The Hope/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("START!!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("QUIT!!!");
+        Application.Quit();
+    }
+}
