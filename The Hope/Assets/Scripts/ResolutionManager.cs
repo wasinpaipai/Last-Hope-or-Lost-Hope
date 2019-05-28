68eb85f0e61ac0f1e6595bf5b4b224523af2e610
The buddy_G's <59070144@it.kmitl.ac.th>
2019-05-28 15:12:19 +0700

    Add Opening Scene
    ใส่หน้าเมนู บวกกับซาวน์ประกอบ

---
+++ The Hope/Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionManager : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public void SetWidth(int newWidth)
+    {
+        width = newWidth;
+    }
+
+    public void SetHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+
+    public void SetRes()
+    {
+        Debug.Log("Setting Screen Complete!!!");
+        Screen.SetResolution(width, height, false);
+    }
+}
